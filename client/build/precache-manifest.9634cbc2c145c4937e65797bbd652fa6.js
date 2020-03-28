692172bb8db4f516797765956b3dfbffb7afa365
Anıl Asım Bilen <abilen18@ku.edu.tr>
2020-03-28 19:59:49 +0300

    build added

---
+++ client/build/precache-manifest.9634cbc2c145c4937e65797bbd652fa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b52937ef88cd0eb6548451ef49ef14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f2cab1b008c9bf2a78",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c08e6e164f03923602fc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e2f2cab1b008c9bf2a78",
+    "url": "/static/js/2.64558fee.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.64558fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08e6e164f03923602fc",
+    "url": "/static/js/main.238144ae.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
